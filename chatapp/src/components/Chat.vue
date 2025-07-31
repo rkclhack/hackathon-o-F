6c9b4a206d7e76aea775ad4418625e7ce4272b0f
yoshi0319 <k737025@kansai-u.ac.jp>
2025-07-31 10:27:12 +0900

    コメントの新規コメントを上に来るように変更。

--- chatapp/src/components/Chat.vue
+++ chatapp/src/components/Chat.vue
@@ -48,17 +48,17 @@ const onMemo = () => {
 // #region socket event handler
 // サーバから受信した入室メッセージ画面上に表示する
 const onReceiveEnter = (data) => {
-  chatList.push(data)
+  chatList.unshift(data)
 }
 
 // サーバから受信した退室メッセージを受け取り画面上に表示する
 const onReceiveExit = (data) => {
-  chatList.push(data)
+  chatList.unshift(data)
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
 const onReceivePublish = (data) => {
-  chatList.push(data)
+  chatList.unshift(data)
 }
 // #endregion
 
